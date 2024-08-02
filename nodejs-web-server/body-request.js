91833ba3be809a8ec661c9df05d6678af79149e5
Iammm <138758217+ilhaaam24@users.noreply.github.com>
2024-08-02 09:47:36 +0700

    make body request

---
+++ nodejs-web-server/body-request.js
@@ -0,0 +1,34 @@
+const http = require("http");
+
+const requestListener = (req, res) => {
+  res.setHeader("Content-Type", "text/html");
+
+  res.statusCode = 200;
+
+  const { method } = req;
+
+  if (method === "GET") {
+    res.end("<h1>Halo!!</h1>");
+  }
+  if (method === "POST") {
+    let body = [];
+
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+
+    req.on("end", () => {
+      body = Buffer.concat(body).toString();
+      const { name } = JSON.parse(body);
+      res.end(`<h1>Haii ${name}!</h1>`);
+    });
+  }
+};
+
+const server = http.createServer(requestListener);
+const port = 5000;
+const host = "localhost";
+
+server.listen(port, host, () => {
+  console.log(`Server berjalan pada http://${host}:${port}`);
+});
