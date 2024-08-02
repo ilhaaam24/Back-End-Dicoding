4955ca34e56a4ecc2384ae01cf98f0045ebf9c93
Iammm <138758217+ilhaaam24@users.noreply.github.com>
2024-08-02 10:11:41 +0700

    make routing request

---
+++ nodejs-web-server/routing-request.js
@@ -0,0 +1,44 @@
+const http = require("http");
+
+const requestListener = (req, res) => {
+  res.setHeader("Content-Type", "text/html");
+
+  res.statusCode = 200;
+  const { method, url } = req;
+
+  if (url === "/") {
+    if (method === "GET") {
+      res.end(`<h1>ini adalah homepage</h1>`);
+    } else {
+      res.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
+    }
+  } else if (url === "/about") {
+    if (method === "GET") {
+      res.end(`<h1>ini adalah halaman about</h1>`);
+    } else if (method === "POST") {
+      let body = [];
+
+      req.on("data", (chunk) => {
+        body.push(chunk);
+      });
+
+      req.on("end", () => {
+        body = Buffer.concat(body).toString();
+        const { name } = JSON.parse(body);
+        res.end(`<h1>Haii ${name}, ini adalah halaman about</h1>`);
+      });
+    } else {
+      res.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
+    }
+  } else {
+    res.end(`<h1>Halaman tidak ditemukan!</h1>`);
+  }
+};
+
+const server = http.createServer(requestListener);
+const port = 5000;
+const host = "localhost";
+
+server.listen(port, host, () => {
+  console.log(`Server berjalan pada http://${host}:${port}`);
+});
