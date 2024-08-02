ef811ad27c13c6a77acd7a795a24c65c27b77a0b
Iammm <138758217+ilhaaam24@users.noreply.github.com>
2024-08-02 09:25:21 +0700

    make opsional method get,post, put & delete

--- nodejs-web-server/server.js
+++ nodejs-web-server/server.js
@@ -1,17 +1,30 @@
 const http = require("http");
 
-const requestListener =(req, res) => {
-   res.setHeader("Content-Type", "text/html");
+const requestListener = (req, res) => {
+  res.setHeader("Content-Type", "text/html");
 
   res.statusCode = 200;
-  res.end('<h1>Halo HTTP Server!</h1>')
-};
 
+  const { method } = req;
+  
+  if (method === "GET") {
+    res.end("<h1>Halo!!</h1>");
+  }
+  if (method === "POST") {
+    res.end("<h1>HAIIII!!</h1>");
+  }
+  if (method === "PUT") {
+    res.end("<h1>Bonjour!!</h1>");
+  }
+  if (method === "DELETE") {
+    res.end("<h1>Salam!!</h1>");
+  }
+};
 
 const server = http.createServer(requestListener);
 const port = 5000;
 const host = "localhost";
 
-server.listen(port, host, () =>{
-  console.log(`Server berjalan pada http://${host}:${port}`)
-})
+server.listen(port, host, () => {
+  console.log(`Server berjalan pada http://${host}:${port}`);
+});
