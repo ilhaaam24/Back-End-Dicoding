406a3b32d3b1963a5fcd2ed4ecb24f36970578bb
Iammm <138758217+ilhaaam24@users.noreply.github.com>
2024-08-03 22:32:44 +0700

    -

---
+++ nodejs-consume-data/server.js
@@ -0,0 +1,67 @@
+const Hapi = require("@hapi/hapi");
+const got = require("got");
+
+const { ORDER_SERVICE_PORT = 4000, USER_SERVICE_PORT = 5000 } = process.env;
+
+const orderService = `http://localhost:${ORDER_SERVICE_PORT}`;
+const userService = `http://localhost:${USER_SERVICE_PORT}`;
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 3000,
+    host: "localhost",
+  });
+
+  server.route([
+    {
+      method: "GET",
+      path: "/{id}",
+      handler: async (request, h) => {
+        const { id } = request.params;
+
+        try {
+          const [order, user] = await Promise.all([got(`${orderService}/${id}`).json(), got(`${userService}/${id}`).json()]);
+
+          return {
+            id: order.id,
+            menu: order.menu,
+            user: user.name,
+          };
+
+
+            // return untuk tampilan html
+          //  const htmlResponse = `
+          //   <html>
+          //     <head>
+          //       <title>Order Details</title>
+          //     </head>
+          //     <body>
+          //       <h1>Order Details</h1>
+          //       <p><strong>Order ID:</strong> ${order.id}</p>
+          //       <p><strong>Menu:</strong> ${order.menu}</p>
+          //       <p><strong>User:</strong> ${user.name}</p>
+          //     </body>
+          //   </html>
+          // `;
+
+          //  return h.response(htmlResponse).type("text/html");
+        } catch (error) {
+          if (!error.response) throw error;
+          if (error.response.statusCode === 400) {
+            return h.response({ message: "bad request" }).code(400);
+          }
+          if (error.response.statusCode === 404) {
+            return h.response({ message: "not found" }).code(404);
+          }
+
+          throw error;
+        }
+      },
+    },
+  ]);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
